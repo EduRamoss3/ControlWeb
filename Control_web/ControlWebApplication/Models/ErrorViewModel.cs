2880d2b8ddee7dba81ebe36a19449fd548c74158
Eduardo Ramos <eduardoramossilva@live.com>
2024-03-03 22:16:50 -0300

    default

---
+++ Control_web/ControlWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ControlWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
